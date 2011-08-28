331a3eeba0003c00ef2d065ca3e4b96fc8d778d6
Derek Stegelman <dstegelman@gmail.com>
2011-08-28 18:30:32 -0500

    Fix for middleware

--- apps/api_docs/middleware.py
+++ apps/api_docs/middleware.py
@@ -1,6 +1,7 @@
 class ContentTypeMiddleware(object):
 
     def process_request(self, request):
-        if request.META['CONTENT_TYPE'] == 'application/x-www-form-urlencoded; charset=UTF-8':
+        content_type = getattr(request.META, "CONTENT_TYPE", "")
+        if content_type == 'application/x-www-form-urlencoded; charset=UTF-8':
             request.META['CONTENT_TYPE'] = 'application/x-www-form-urlencoded'
         return None
