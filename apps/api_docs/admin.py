cac8a6fcb4a172d939ee76d17cf6a63be0adaf46
Derek Stegelman <dstegelman@gmail.com>
2011-08-29 08:27:02 -0500

    Bug fixes, addresses issue #1.  Add published to methods so that they can be created without being displayed.

--- apps/api_docs/admin.py
+++ apps/api_docs/admin.py
@@ -5,9 +5,25 @@ admin.site.register(APIDoc)
 admin.site.register(APIObject)
     
 class APIMethodAdmin(admin.ModelAdmin):
-    list_display = ('name', 'type', 'api_object', 'api_url')
+    list_display = ('name', 'type', 'api_object', 'api_url', 'published')
     save_on_top = True
-    filter_vertical = ('parameter',)
+    actions = ['publish', 'un_publish']
+    
+    def publish(self, request, queryset):
+        rows = queryset.update(published=True)
+        if rows == 1:
+            message_bit = "1 method was"
+        else:
+            message_bit = "%s methods were" % rows
+        self.message_user(request, "%s successfully published." % message_bit)
+        
+    def un_publish(self, request, queryset):
+        rows = queryset.update(published=False)
+        if rows == 1:
+            message_bit = "1 method was"
+        else:
+            message_bit = "%s methods were" % rows
+        self.message_user(request, "%s successfully un-published." % message_bit)
     
     
 admin.site.register(Parameter)
--- apps/api_docs/middleware.py
+++ apps/api_docs/middleware.py
@@ -1,7 +1,12 @@
 class ContentTypeMiddleware(object):
 
     def process_request(self, request):
-        content_type = getattr(request.META, "CONTENT_TYPE", "")
-        if content_type == 'application/x-www-form-urlencoded; charset=UTF-8':
-            request.META['CONTENT_TYPE'] = 'application/x-www-form-urlencoded'
-        return None
+        
+        try:
+            if request.META['CONTENT_TYPE']:
+                if request.META['CONTENT_TYPE'] == 'application/x-www-form-urlencoded; charset=UTF-8':
+                    request.META['CONTENT_TYPE'] = 'application/x-www-form-urlencoded'
+            return None
+        except:
+            pass
+            return None
