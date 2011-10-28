475c1fcee2304bef78b051945276d20632d280b8
Derek Stegelman <dstegelman@gmail.com>
2011-10-28 10:53:01 -0500

    Err, moving backwards for compatabilities sake.

--- api_docs/views.py
+++ api_docs/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from api_docs.models import *
+from django.template import RequestContext
 
 def home(request):
     context = {'apis':APIDoc.objects.all()}
-    return render(request, 'api_docs/apis.html', context)
+    return render_to_response('api_docs/apis.html', context, context_instance=RequestContext(request))
     
 def build_js(request):
     context = {'apis': APIDoc.objects.all()}
-    return render(request, 'api_docs/docs.js', context)
+    return render_to_response('api_docs/docs.js', context, context_instance=RequestContext(request))
