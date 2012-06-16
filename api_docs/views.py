ef96f4a287b98c8ae05b494096ba16393aa9cf23
Derek Stegelman <dstegelman@gmail.com>
2012-06-16 10:51:03 -0500

    soap call

--- api_docs/views.py
+++ api_docs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from api_docs.models import *
+
 
 def home(request):
     context = {'apis':APIDoc.objects.all()}
@@ -9,9 +10,17 @@ def build_js(request):
     context = {'apis': APIDoc.objects.all()}
     return render(request, 'api_docs/docs.js', context)
 
-def soap_call(request, api_method_id=None):
+def soap_call(request, soap_method_id):
     """
     Makes the SOAP based api call, and returns the dump of xml.  This is called via ajax
     by the browser to appear that the call was async.
     """
+    # Grab all vars
+    #for param in
+    context = {}
+    method = get_object_or_404(APIMethod, pk=soap_method_id)
+    param_string = ""
+    for param in method.parameter.all():
+        param_string = param_string + "%s=%s," % (param.name, request.GET.get(param.name))
+        print param_string
     return render(request, "api_docs/apis.html", context)
